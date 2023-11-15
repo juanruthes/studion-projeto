e0abf0ee124d62b6bbe61f2c0072cb256b4820c5
Juan <juanruthes@outlook.com>
2023-11-14 22:47:12 -0300

    Aula 11 completa

--- src/script.js
+++ src/script.js
@@ -9,4 +9,19 @@ $(document).ready(function () {
       '<button type="button" class="slick-next slider__next">Next</button>',
   };
   $(".slider").slick(slickOptions);
+
+  $(".footer__form-button").on("click", () => {
+    const email = $("#email").val();
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "juanruthes2@gmail.com",
+      Password: "63967B00AAD0FEC8924AAABF8E15DC7B944F",
+      To: "juanruthes2@gmail.com",
+      From: email,
+      Subject: "Por favor me adicione na newsletter",
+      Body: `Olá,
+      Eu gostaria de ser adicionado na newsletter do site.
+      Meu e-mail é ${email}`,
+    }).then((message) => alert(message));
+  });
 });
